71a77736a4241479b3cffca205bba17d8ed0139a
11Samo <samuel.kurnas@gmail.com>
2023-03-11 22:49:58 +0100

    Finished with desktop functionalities

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ pledgeHeadings.forEach((heading) => {
         .closest(".modal-card-row")
         .querySelector(".radio-btn-input");
       radioBtn.checked = true;
-      removeAllColorBorders();
+      removeAllColorBorders(heading);
       addColorBorders(heading);
     }
   });
@@ -62,12 +62,53 @@ pledgeHeadings.forEach((heading) => {
 const addColorBorders = (btn) => {
   const cardEl = btn.closest(".modal-card");
   cardEl.classList.add("highlight-borders");
+
+  if (!cardEl.classList.contains("no-reward")) {
+    addPledgeForm(cardEl);
+  }
 };
 
 const removeAllColorBorders = () => {
+  inputElements = document.querySelectorAll(".enter-pledge");
+
+  inputElements.forEach((element) => {
+    element.remove();
+  });
+
   const highlightEls = document.querySelectorAll(".highlight-borders");
 
   highlightEls.forEach((element) => {
     element.classList.remove("highlight-borders");
+  });
+};
+
+const addPledgeForm = (rootElement) => {
+  const div = document.createElement("div");
+  div.classList.add("enter-pledge");
+  div.innerHTML = `
+    <p>Enter your pledge</p>
+    <label class="input" for="radio-btn-2-id">
+      <input
+        class="input-pledge"
+        type="number"
+        id="radio-btn-2-id"
+        name="stand-type"
+      />
+      <span class="dollar-sign">$</span>
+    </label>
+    <button class="btn">Continue</button>
+  `;
+  rootElement.appendChild(div);
+
+  const continueBtn = rootElement.querySelector(".btn");
+  continueBtn.addEventListener("click", (event) => {
+    const leftNumber = event.target
+      .closest(".modal-card")
+      .querySelector(".left-number");
+    let number = +leftNumber.innerHTML;
+    leftNumber.innerHTML = --number;
+
+    modalSupport.classList.remove("show");
+    modalComplete.classList.add("show");
   });
 };
