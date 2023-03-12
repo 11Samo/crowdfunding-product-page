906cff9215a158f18d90fb5d3fa06e7f3557847f
11Samo <samuel.kurnas@gmail.com>
2023-03-12 20:58:05 +0100

    Rewrite page for responsivness first part (header and menu) finished

--- script.js
+++ script.js
@@ -1,114 +1,128 @@
-const backThisProjectBtn = document.getElementById("back-this-project-btn-id");
-const bookmarkBtn = document.getElementById("bookmark-id");
-const bookmarkText = document.querySelector(".bookmark");
-const rewardBtns = document.querySelectorAll(".card-btn");
-
-const modalComplete = document.getElementById("modal-complete-id");
-const gotItBtn = document.getElementById("got-it-btn-id");
-
-const modalSupport = document.getElementById("modal-support-id");
-const closeModalBtn = document.querySelector(".close-modal-icon");
-const radioBtns = document.querySelectorAll(".radio-btn-input");
-const pledgeHeadings = document.querySelectorAll(".pledge-heading");
-
-//#region LISTENERES
-backThisProjectBtn.addEventListener("click", () => {
-  modalSupport.classList.add("show");
-});
-
-closeModalBtn.addEventListener("click", () => {
-  modalSupport.classList.remove("show");
-});
-
-bookmarkBtn.addEventListener("click", () => {
-  if (bookmarkBtn.checked) {
-    bookmarkText.innerHTML = "Bookmarked";
-  } else {
-    bookmarkText.innerHTML = "Bookmark";
-  }
-});
-
-rewardBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    modalComplete.classList.add("show");
+///////////////////
+// MENU
+//////////////////
+const toggleButton = document.querySelector(".navbar-toggle");
+console.log(toggleButton);
+const navbarLinks = document.querySelectorAll(".navbar-links");
+toggleButton.addEventListener("click", (event) => {
+  const menuPart = event.target.closest(".menu-part");
+  menuPart.classList.toggle("active");
+  navbarLinks.forEach((link) => {
+    link.classList.toggle("active");
   });
 });
 
-gotItBtn.addEventListener("click", () => {
-  modalComplete.classList.remove("show");
-});
+// const backThisProjectBtn = document.getElementById("back-this-project-btn-id");
+// const bookmarkBtn = document.getElementById("bookmark-id");
+// const bookmarkText = document.querySelector(".bookmark");
+// const rewardBtns = document.querySelectorAll(".card-btn");
 
-radioBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    removeAllColorBorders();
-    addColorBorders(btn);
-  });
-});
+// const modalComplete = document.getElementById("modal-complete-id");
+// const gotItBtn = document.getElementById("got-it-btn-id");
 
-pledgeHeadings.forEach((heading) => {
-  heading.addEventListener("click", () => {
-    if (!heading.closest(".modal-card").classList.contains("disabled")) {
-      let radioBtn = heading
-        .closest(".modal-card-row")
-        .querySelector(".radio-btn-input");
-      radioBtn.checked = true;
-      removeAllColorBorders(heading);
-      addColorBorders(heading);
-    }
-  });
-});
-//#endregion
+// const modalSupport = document.getElementById("modal-support-id");
+// const closeModalBtn = document.querySelector(".close-modal-icon");
+// const radioBtns = document.querySelectorAll(".radio-btn-input");
+// const pledgeHeadings = document.querySelectorAll(".pledge-heading");
 
-const addColorBorders = (btn) => {
-  const cardEl = btn.closest(".modal-card");
-  cardEl.classList.add("highlight-borders");
+// //#region LISTENERES
+// backThisProjectBtn.addEventListener("click", () => {
+//   modalSupport.classList.add("show");
+// });
 
-  if (!cardEl.classList.contains("no-reward")) {
-    addPledgeForm(cardEl);
-  }
-};
+// closeModalBtn.addEventListener("click", () => {
+//   modalSupport.classList.remove("show");
+// });
 
-const removeAllColorBorders = () => {
-  inputElements = document.querySelectorAll(".enter-pledge");
+// bookmarkBtn.addEventListener("click", () => {
+//   if (bookmarkBtn.checked) {
+//     bookmarkText.innerHTML = "Bookmarked";
+//   } else {
+//     bookmarkText.innerHTML = "Bookmark";
+//   }
+// });
 
-  inputElements.forEach((element) => {
-    element.remove();
-  });
+// rewardBtns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     modalComplete.classList.add("show");
+//   });
+// });
 
-  const highlightEls = document.querySelectorAll(".highlight-borders");
+// gotItBtn.addEventListener("click", () => {
+//   modalComplete.classList.remove("show");
+// });
 
-  highlightEls.forEach((element) => {
-    element.classList.remove("highlight-borders");
-  });
-};
+// radioBtns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     removeAllColorBorders();
+//     addColorBorders(btn);
+//   });
+// });
 
-const addPledgeForm = (rootElement) => {
-  const div = document.createElement("div");
-  div.classList.add("enter-pledge");
-  div.innerHTML = `
-    <p>Enter your pledge</p>
-    <label class="input" for="radio-btn-2-id">
-      <input
-        class="input-pledge"
-        type="number"
-        id="radio-btn-2-id"
-        name="stand-type"
-      />
-      <span class="dollar-sign">$</span>
-    </label>
-    <button class="btn">Continue</button>
-  `;
-  rootElement.appendChild(div);
+// pledgeHeadings.forEach((heading) => {
+//   heading.addEventListener("click", () => {
+//     if (!heading.closest(".modal-card").classList.contains("disabled")) {
+//       let radioBtn = heading
+//         .closest(".modal-card-row")
+//         .querySelector(".radio-btn-input");
+//       radioBtn.checked = true;
+//       removeAllColorBorders(heading);
+//       addColorBorders(heading);
+//     }
+//   });
+// });
+// //#endregion
 
-  const continueBtn = rootElement.querySelector(".btn");
-  continueBtn.addEventListener("click", (event) => {
-    const leftNumber = event.target
-      .closest(".modal-card")
-      .querySelector(".left-number");
-    let number = +leftNumber.innerHTML;
-    leftNumber.innerHTML = --number;
+// const addColorBorders = (btn) => {
+//   const cardEl = btn.closest(".modal-card");
+//   cardEl.classList.add("highlight-borders");
 
-    modalSupport.classList.remove("show");
-    modalComplete.classList.add("show");
-  });
-};
+//   if (!cardEl.classList.contains("no-reward")) {
+//     addPledgeForm(cardEl);
+//   }
+// };
+
+// const removeAllColorBorders = () => {
+//   inputElements = document.querySelectorAll(".enter-pledge");
+
+//   inputElements.forEach((element) => {
+//     element.remove();
+//   });
+
+//   const highlightEls = document.querySelectorAll(".highlight-borders");
+
+//   highlightEls.forEach((element) => {
+//     element.classList.remove("highlight-borders");
+//   });
+// };
+
+// const addPledgeForm = (rootElement) => {
+//   const div = document.createElement("div");
+//   div.classList.add("enter-pledge");
+//   div.innerHTML = `
+//     <p>Enter your pledge</p>
+//     <label class="input" for="radio-btn-2-id">
+//       <input
+//         class="input-pledge"
+//         type="number"
+//         id="radio-btn-2-id"
+//         name="stand-type"
+//       />
+//       <span class="dollar-sign">$</span>
+//     </label>
+//     <button class="btn">Continue</button>
+//   `;
+//   rootElement.appendChild(div);
+
+//   const continueBtn = rootElement.querySelector(".btn");
+//   continueBtn.addEventListener("click", (event) => {
+//     const leftNumber = event.target
+//       .closest(".modal-card")
+//       .querySelector(".left-number");
+//     let number = +leftNumber.innerHTML;
+//     leftNumber.innerHTML = --number;
+
+//     modalSupport.classList.remove("show");
+//     modalComplete.classList.add("show");
+//   });
+// };
